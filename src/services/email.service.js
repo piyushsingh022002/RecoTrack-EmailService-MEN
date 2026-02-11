0a05e9486ebc03d2682ea0c371ed00adb6ccd36e
Piyush <piyushsingh6521@gmail.com>
2026-02-11 23:26:01 +0530

    Service Name changed

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -4,7 +4,7 @@ const EmailLog = require('../models/emailLog.model');
 exports.sendEmail = async ({to, subject, htmlContent}) => {
     console.log('Preparing to send email to:', to);
     const sendSmtpEmail = {
-        sender: {email : "workspace.piyush01@gmail.com", name : "Piyush Singh"},
+        sender: {email : "workspace.piyush01@gmail.com", name : "RecoTrack Team 📚"},
         to: [{email : to}],
         subject,
         htmlContent
